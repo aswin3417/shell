3a2e139e4273ec2be428e1b6b9709895d55052d0
aswin3417 <aswinac090@gmail.com>
2025-05-23 07:19:43 +0530

    Update triangle or not.sh

--- triangle or not.sh
+++ triangle or not.sh
@@ -14,3 +14,8 @@ read c
 fi
 if [ `expr $a + $b` -le $c -o `expr $a + $c` -le $b -o `expr $b + $c` -le $a ]
 #if [ `expr $a + $b` -le $c ]
+then
+echo "$a $b $c cannot be sides of triangle"
+else
+echo "$a $b $c can be sides of triangle"
+fi
